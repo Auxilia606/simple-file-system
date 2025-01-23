bc5b353fa81a097d22e6e561f0736f6a029adb8e
youngtaek_hong <youngtaek_hong@dweax.com>
2025-01-23 16:18:19 +0900

    feat(material-ui-ssr): material ui nextjs 이식

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -5,8 +5,11 @@ import MainLogo from "@/widget/MainLogo";
 import MainSideAdd from "@/widget/MainSideAdd";
 import MainSideBar from "@/widget/MainSideBar";
 import { Box, Stack } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
 
 import "./globals.css";
+import theme from "@/shared/utils/theme";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,31 +36,35 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Box
-          sx={{ height: "100%" }}
-          display="grid"
-          gridTemplateColumns="auto 1fr auto auto auto"
-          gridTemplateRows="auto auto 1fr"
-          gridTemplateAreas={`
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{ height: "100%" }}
+              display="grid"
+              gridTemplateColumns="auto 1fr auto auto auto"
+              gridTemplateRows="auto auto 1fr"
+              gridTemplateAreas={`
           "logo head head head c-sh"
           "newb vwgr d--s c-sw c-sh"
           "navp vwgr d--s c-sw c-sh"
         `}
-        >
-          <Stack gridArea="logo">
-            <MainLogo />
-          </Stack>
-          <Stack gridArea="head">
-            <MainHeader />
-          </Stack>
-          <Stack gridArea="newb">
-            <MainSideAdd />
-          </Stack>
-          <Stack gridArea="navp">
-            <MainSideBar />
-          </Stack>
-          <Stack gridArea="vwgr">{children}</Stack>
-        </Box>
+            >
+              <Stack gridArea="logo">
+                <MainLogo />
+              </Stack>
+              <Stack gridArea="head">
+                <MainHeader />
+              </Stack>
+              <Stack gridArea="newb">
+                <MainSideAdd />
+              </Stack>
+              <Stack gridArea="navp">
+                <MainSideBar />
+              </Stack>
+              <Stack gridArea="vwgr">{children}</Stack>
+            </Box>
+          </ThemeProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
